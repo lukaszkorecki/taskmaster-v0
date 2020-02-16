c37e6a2d068a046cbdfe7f612ff4516c42922b19
Łukasz Korecki <lukasz@korecki.me>
2020-02-15 21:51:11 -0800

    Clean up scratch code

--- src/taskmaster/scratch.clj
+++ src/taskmaster/scratch.clj
@@ -1,28 +1,30 @@
-(require '[taskmaster.scratch :as s]
+(require '[taskmaster.connection :as c]
          '[clojure.tools.logging :as log]
          '[utility-belt.sql.helpers :as sql]
          '[taskmaster.core :as q] :reload)
 
-(def c1 (.start (s/make-conn)))
-(def c2 (.start (s/make-conn)))
+(def c1 (.start (c/make-one)))
+(def c2 (.start (c/make-one)))
 
 
 (q/create-jobs-table! c1)
+
+(defn notify-callback [args]
+  (log/info (q/queue-size c2 {:queue-name "test_queue"}))
+  (if (empty? args)
+    (log/warn "no jobs")
+    (let [jobs (q/lock! c2 {:queue-name "test_queue"})]
+      (mapv (fn [job]
+              (log/infof "job %s" (:id job))
+              (q/delete-job! c2 {:id (:id job)})
+              (q/unlock! c2 {:id (:id job)})) jobs))))
 
 (def listener
   (Thread.
    (fn []
      (q/listen c1 {:queue-name "test_queue"
-                   :callback (fn [args]
-                               (log/info (q/queue-size c2 {:queue-name "test_queue"}))
-                               (if (empty? args)
-                                 (log/info "no jobs")
-
-                                 (let [jobs (q/lock! c2 {:queue-name "test_queue"})]
-                                   (mapv (fn [job]
-                                           (log/infof "job %s" job)
-                                           (q/delete-job! c2 {:id (:id job)})
-                                           (q/unlock! c2 {:id (:id job)})) jobs))))}))))
+                   :callback notify-callback}))
+   "listener-1"))
 
 
 
@@ -30,20 +32,17 @@
   (Thread.
    (fn []
      (q/listen c1 {:queue-name "test_queue"
-                   :callback (fn [args]
-                               (log/info (q/queue-size c2 {:queue-name "test_queue"}))
-                               (if (empty? args)
-                                 (log/info "no jobs")
-
-                                 (let [jobs (q/lock! c2 {:queue-name "test_queue"})]
-                                   (mapv (fn [job]
-                                           (log/infof "job %s" job)
-                                           (q/delete-job! c2 {:id (:id job)})
-                                           (q/unlock! c2 {:id (:id job)})) jobs))))}))))
+                   :callback notify-callback }))
+   "listener-2"
+   ))
 
 (.stop listener)
+(.stop listener2)
 (q/put! c2 {:queue-name "test_queue" :payload {:hi :there :time (str (java.time.LocalDateTime/now))}})
 (.start listener)
 (.start listener2)
 
 (.stop listener2)
+
+
+(q/delete-all! c2 {:queue-name "test_queue"})
