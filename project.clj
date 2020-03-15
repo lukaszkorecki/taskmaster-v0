78787cf4a8433c2d3675026321b435c46afbff8a
Łukasz Korecki <lukasz@korecki.me>
2020-03-15 13:37:31 -0700

    update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[nomnom/utility-belt.sql "1.0.0.beta1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.5.0"]
-                                  [com.stuartsierra/component "0.4.0"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.logging "1.0.0"]
+                                  [com.stuartsierra/component "1.0.0"]
                                   [ch.qos.logback/logback-classic "1.2.3"]
                                   [nomnom/utility-belt "1.2.2"]
                                   [org.clojure/clojure "1.10.1"]]
