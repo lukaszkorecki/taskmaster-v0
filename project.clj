f09a43f26cc5276a5e8603cc9dd7b45ade4cfae3
Łukasz Korecki <lukasz@korecki.me>
2020-08-01 09:55:27 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/lukaszkorecki/taskmaster"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[nomnom/utility-belt.sql "1.0.0.beta1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.logging "1.0.0"]
+  :dependencies [[nomnom/utility-belt.sql "1.0.0.beta4"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.logging "1.1.0"]
                                   [com.stuartsierra/component "1.0.0"]
                                   [ch.qos.logback/logback-classic "1.2.3"]
-                                  [nomnom/utility-belt "1.2.2"]
+                                  [nomnom/utility-belt "1.3.0"]
                                   [org.clojure/clojure "1.10.1"]]
                    :resource-paths ["dev-resources"]}}
   :deploy-repositories {"clojars" {:sign-releases false
