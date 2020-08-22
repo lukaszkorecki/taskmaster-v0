385266d76e2ac53715f39afee8837e8d122a0d4e
Łukasz Korecki <lukasz@korecki.me>
2020-08-22 15:09:02 -0700

    better dependnecies

--- project.clj
+++ project.clj
@@ -3,11 +3,17 @@
   :url "https://github.com/lukaszkorecki/taskmaster"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[nomnom/utility-belt.sql "1.0.0.beta4"]]
+  :dependencies [[seancorfield/next.jdbc "1.1.582"]
+                 [org.postgresql/postgresql "42.2.16"]
+                 [cheshire "5.10.0"]
+                 [clj-time "0.15.2"]
+
+                 [hikari-cp "2.11.0"]
+
+                 [nomnom/utility-belt.sql "1.0.0.beta4" :exclusions [seancorfield/next.jdbc hikari-cp org.postgresql/postgresql]]]
   :profiles {:dev {:dependencies [[org.clojure/tools.logging "1.1.0"]
                                   [com.stuartsierra/component "1.0.0"]
                                   [ch.qos.logback/logback-classic "1.2.3"]
-                                  [nomnom/utility-belt "1.3.0"]
                                   [org.clojure/clojure "1.10.1"]]
                    :resource-paths ["dev-resources"]}}
   :deploy-repositories {"clojars" {:sign-releases false
